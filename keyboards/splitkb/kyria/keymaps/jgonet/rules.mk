4094a013afb8d4c4a31c652930b16154ac3a8698
Jakub Gonet <jakub.gonet@swmansion.com>
2021-10-04 20:52:56 +0200

    Add jgonet keymap

---
+++ keyboards/splitkb/kyria/keymaps/jgonet/rules.mk
@@ -0,0 +1,6 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+ENCODER_ENABLE = no       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
+LEADER_ENABLE = yes        # Enable the Leader Key feature
+MOUSEKEY_ENABLE = no
