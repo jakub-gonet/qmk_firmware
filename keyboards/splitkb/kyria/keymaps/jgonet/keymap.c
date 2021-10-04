7b26ead1a9d9226d41d776cc2e0de63258d0262f
Jakub Gonet <jakub.gonet@swmansion.com>
2021-10-04 21:39:02 +0200

    Fix layers

--- keyboards/splitkb/kyria/keymaps/jgonet/keymap.c
+++ keyboards/splitkb/kyria/keymaps/jgonet/keymap.c
@@ -19,7 +19,6 @@ enum layers {
     QWERTY = 0,
     LOWER,
     RAISE,
-    NAV,
 };
 
 enum custom_keycodes {
@@ -38,14 +37,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | LGUI   |   Z  |   X  |   C  |   V  |   B  |  [ { |  Tab |  | LEAD |  ] } |   N  |   M  | ,  < | . >  | /  ? |  RGUI  |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        | LCTL | Alt  |      | Space| Enter|  | Bspc | Space|      | AltR | RCTL |
- *                        |      |      | Lower|      |      |  |      | Nav  | Raise|      |      |
+ *                        |      |      | Lower|      |      |  |      |      | Raise|      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [QWERTY] = LAYOUT(
       KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLASH,
       KC_LSFT, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       KC_LGUI, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRACKET, KC_TAB,        KC_LEAD, KC_RBRACKET,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RGUI,
-            KC_LCTL, KC_LALT, MO(LOWER),               KC_SPC,      KC_ENT,        KC_BSPC, LT(NAV, KC_SPC), MO(RAISE), KC_RALT, KC_RCTL
+            KC_LCTL, KC_LALT, MO(LOWER),               KC_SPC,      KC_ENT,        KC_BSPC, KC_SPC, MO(RAISE), KC_RALT, KC_RCTL
     ),
 /*
  * Lower Layer: Numbers, Symbols
@@ -63,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [LOWER] = LAYOUT(
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     _______,
-      _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LCBR,  KC_RCBR,  _______,
+      _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,  KC_RPRN,  _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MINUS, KC_EQUAL, _______,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
@@ -82,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        |      |      | Lower|      |      |  |      | Nav  | Raise|      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
-    [NAV] = LAYOUT(
+    [RAISE] = LAYOUT(
       _______, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_SLCK,                                     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
       _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_CAPS,                                     _______, KC_F4,   KC_F5,   KC_F6,   KC_F11,  _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F12,  _______,
@@ -151,9 +150,6 @@ static void render_status(void) {
         case RAISE:
             oled_write_P(PSTR("Raise\n"), false);
             break;
-        case NAV:
-            oled_write_P(PSTR("Navigation\n"), false);
-            break;
         default:
             oled_write_P(PSTR("Undefined\n"), false);
     }
@@ -166,6 +162,10 @@ static void render_status(void) {
 }
 
 void oled_task_user(void) {
-    render_status();
+    if (is_keyboard_master()) {
+        render_status(); // Renders the current keyboard state (layer, lock, caps, scroll, etc)
+    } else {
+        oled_off();
+    }
 }
 #endif
