dcc31f16d62135bc67376e12b41b0ac547c53105
Jakub Gonet <jakub.gonet@swmansion.com>
2021-10-04 21:08:56 +0200

    Add backslash instead of pipe

--- keyboards/splitkb/kyria/keymaps/jgonet/keymap.c
+++ keyboards/splitkb/kyria/keymaps/jgonet/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [QWERTY] = LAYOUT(
-      KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PIPE,
+      KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLASH,
       KC_LSFT, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       KC_LGUI, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRACKET, KC_TAB,        KC_LEAD, KC_RBRACKET,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RGUI,
             KC_LCTL, KC_LALT, MO(LOWER),               KC_SPC,      KC_ENT,        KC_BSPC, LT(NAV, KC_SPC), MO(RAISE), KC_RALT, KC_RCTL
