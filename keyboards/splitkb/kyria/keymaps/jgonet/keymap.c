b2d9ca7eb4794087d8e5f8b820c80738be951455
Jakub Gonet <jakub.gonet@swmansion.com>
2021-10-05 11:49:55 +0200

    Swap backspace with enter and move - _

--- keyboards/splitkb/kyria/keymaps/jgonet/keymap.c
+++ keyboards/splitkb/kyria/keymaps/jgonet/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | LGUI   |   Z  |   X  |   C  |   V  |   B  |  [ { |  Tab |  | LEAD |  ] } |   N  |   M  | ,  < | . >  | /  ? |  RGUI  |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        | LCTL | Alt  |      | Space| Enter|  | Bspc | Space|      | AltR | RCTL |
+ *                        | LCTL | Alt  |      | Space| Enter|  | Space| Bspc |      | AltR | RCTL |
  *                        |      |      | Lower|      |      |  |      |      | Raise|      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLASH,
       KC_LSFT, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       KC_LGUI, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRACKET, KC_TAB,        KC_LEAD, KC_RBRACKET,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RGUI,
-            KC_LCTL, KC_LALT, MO(LOWER),               KC_SPC,      KC_ENT,        KC_BSPC, KC_SPC, MO(RAISE), KC_RALT, KC_RCTL
+            KC_LCTL, KC_LALT, MO(LOWER),               KC_SPC,      KC_ENT,        KC_SPC, KC_BSPC, MO(RAISE), KC_RALT, KC_RCTL
     ),
 /*
  * Lower Layer: Numbers, Symbols
@@ -54,8 +54,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+                              +------+------+------+------+------+--------|
  * |        |  !   |   @  |  #   |  $   |   %  |                              |   ^  |   &  |   *  |   (  |   )  |        |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |      |  |      |      |      |      |      |  - _ |  = + |        |
- * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
+ * |        |      |      |  - _ |  = + |      |      |      |  |      |      |      |      |      |      |      |        |
+ * `----------------------+--------------------+------+------|  |------+------+------+------+------+----------------------'
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        |      |      | Lower|      |      |  |      | Nav  | Raise|      |      |
  *                        `----------------------------------'  `----------------------------------'
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LOWER] = LAYOUT(
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     _______,
       _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,  KC_RPRN,  _______,
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MINUS, KC_EQUAL, _______,
+      _______, _______, _______, KC_MINUS, KC_EQUAL, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
